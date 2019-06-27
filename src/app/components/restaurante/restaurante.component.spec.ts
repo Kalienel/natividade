17cef36642965b12b516d71385fa989ab4194035
Kalienel <eduardonsramos@gmail.com>
2019-06-27 18:09:48 -0300

    finalização do cardápio

---
+++ src/app/components/restaurante/restaurante.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RestauranteComponent } from './restaurante.component';
+
+describe('RestauranteComponent', () => {
+  let component: RestauranteComponent;
+  let fixture: ComponentFixture<RestauranteComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RestauranteComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RestauranteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
