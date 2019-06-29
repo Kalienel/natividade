46f0051a829ae9114953465b140f5423ec0d6a2f
Kalienel <eduardonsramos@gmail.com>
2019-06-28 22:14:03 -0300

    Estabelecimentos na primeira página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   /*{ path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },*/
-  { path: '', loadChildren: './pages/cardapio/cardapio.module#CardapioPageModule' },
+  { path: '', loadChildren: './pages/estabelecimentos/estabelecimentos.module#EstabelecimentosPageModule' },
   { path: 'estabelecimentos', loadChildren: './pages/estabelecimentos/estabelecimentos.module#EstabelecimentosPageModule' },
   { path: 'cardapio', loadChildren: './pages/cardapio/cardapio.module#CardapioPageModule' },
   { path: 'comprar', loadChildren: './pages/comprar/comprar.module#ComprarPageModule' }
